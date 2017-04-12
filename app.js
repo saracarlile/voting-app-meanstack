834fad09d4b4f7d20368075fedd6dbf698aa770c
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-04-12 10:58:43 -0500

    add Passport.JS for auth and JWT tokens for session management

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 require('./models/Polls');
 require('./models/Options');
 require('./models/Users');
+
+require('./config/passport');
 
 mongoose.connect('mongodb://localhost/votingstateparamtest');
 
@@ -27,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', index);
 app.use('/users', users);
